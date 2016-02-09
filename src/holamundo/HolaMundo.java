6c64f1b62c7aa67be757ff0bb0fb74af8ab23ad2
Allocco14 <achavesl@unal.edu.co>
2016-02-09 15:36:10 -0500

    Update HolaMundo.java

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -11,9 +11,6 @@ package holamundo;
  */
 public class HolaMundo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
